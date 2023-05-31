efb24bd774a05193569c5f03e67e5b97f6a80ba8
zesettler <gromovgrin@gmail.com>
2023-05-31 19:32:40 +0300

    commit

---
+++ app/layout.tsx
@@ -0,0 +1,25 @@
+import './globals.css'
+import AuthContext from './context/AuthContext'
+import ToasterContext from './context/ToasterContext'
+
+export const metadata = {
+  title: 'Messenger',
+  description: 'Messenger Clone',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body>
+        <AuthContext>
+          <ToasterContext />
+          {children}
+        </AuthContext>
+      </body>
+    </html>
+  )
+}
